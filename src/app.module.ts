71c8bc473a9f1be090058a7d4fd765424b262c6d
Yordi <yordi.cruz@gmail.com>
2025-06-29 09:49:31 -0400

    base de datos y docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: '.development.env',
-    isGlobal: true
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities:[],
+      synchronize: false,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
